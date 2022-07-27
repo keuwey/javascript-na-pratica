de462d40a3f42f7a722266cd677ad5067483187e
keuwey <coemgein600673@gmail.com>
2022-07-27 20:10:16 -0300

    fazendo outros testes

--- index.js
+++ index.js
@@ -3,5 +3,16 @@
 // console.log(nomeCompleto);
 let numeroA = 9, numeroB = 3;
 let resultado;
-resultado = numeroA * numeroB;
-console.log(resultado);
+// resultado = numeroA * numeroB;
+// console.log(resultado);
+function podeDirigir(idade){
+	if (idade >= 18) {
+		console.log("Você pode dirigir");
+	}
+	else {
+		console.log("Você não pode dirigir");
+	}
+}
+podeDirigir(17);
+podeDirigir(21);
+podeDirigir(18);
