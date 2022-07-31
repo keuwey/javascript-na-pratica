4e6095c60afbddb9c8f2170d60ee8e44c1ba51e4
keuwey <coemgein600673@gmail.com>
2022-07-31 19:34:20 -0300

    Continuando com os exercícios diários

---
+++ exercicio4.2-1_2.js
@@ -0,0 +1,7 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+const crescente = numbers.sort((a,b) => a-b);
+console.log("\n" + crescente);
+
+let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+const descrescente = numbers.sort((a,b) => b-a);
+console.log("\n" + descrescente);
