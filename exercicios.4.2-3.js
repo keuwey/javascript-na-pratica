ee76975a9b9704b8b2cd6562a70ce1d6d529c541
keuwey <coemgein600673@gmail.com>
2022-08-01 17:18:47 -0300

    Consegui resolver finalmente usando a lógica do @Boltus

--- exercicios.4.2-3.js
+++ exercicios.4.2-3.js
@@ -1,4 +1,12 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (let x = 0; x < numbers.length; x++){
-    if (numbers[x+1]){}
+function multipy(values){
+    let newData = [];
+    valuesLength = values.length - 1;
+    for (let i = 0; i < valuesLength; i++){
+        newData.push(values[i] * values[i+1]);
+    }
+    newData.push(values[valuesLength] * 2);
+    return newData;
 }
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 33, 0, 1, 21];
+let newData = multipy(numbers);
+console.log(newData);
